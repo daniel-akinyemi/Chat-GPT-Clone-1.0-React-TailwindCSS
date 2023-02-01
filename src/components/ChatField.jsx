bcafbca3fa6d0f67c8eca1e40bf060813e39c78a
daniel-akinyemi <danielakinyemi1999@gmail.com>
2023-02-01 14:24:59 +0100

    Chat Interface Added

--- src/components/ChatField.jsx
+++ src/components/ChatField.jsx
@@ -4,10 +4,10 @@ export const ChatField = () => {
   return (
     <div className='w-full'>
         <form className='flex bg-gray-700 w-4/5 px-4 rounded-md items-center mx-auto '>
-            <input type="text" className='bg-gray-700 flex-grow h-12'/>
-            <submit>
+            <input type="text" className='bg-gray-700 flex-grow h-12 outline-none'/>
+            <button>
                 <ArrowTopRightOnSquareIcon className='h-6 bg-gray-700'/>
-            </submit>
+            </button>
         </form>
     </div>
   )
---
+++ src/components/ChatInterfaceGPT.jsx
@@ -0,0 +1,13 @@
+export const ChatInterfaceGPT = () => {
+  return (
+    
+        <div className="chatInterfaceClient">
+            <img 
+           className="rounded-xl mx-0 " 
+           src='https://avatars.githubusercontent.com/u/101728177?v=4'
+           />
+            <p className="chatInterfaceClientMessage"> This is a client test prompt</p>
+        </div>  
+    
+  )
+}
